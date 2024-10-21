5f5a51c9c858ae2cbe58b572227d417458ea0495
William Artero <git@artero.dev>
2024-10-21 20:38:09 +0200

    chore: updated goutline dependencies

--- pkgs/goutline/default.nix
+++ pkgs/goutline/default.nix
@@ -9,11 +9,15 @@ buildGoModule rec {
 		repo = pname;
 		rev = "v${version}";
 		hash = "sha256-Ttk68iNZBKpsJbltpIqPlotYIhOqDG4KghwtIU39iEQ=";
+		postFetch = ''
+			export GOCACHE=$TMPDIR/go-build
+			export GOPATH=$TMPDIR/go
+			export GOMODCACHE=$TMPDIR/go/pkg/mod
+			cd $out
+			${lib.getExe go} mod init github.com/1pkg/goutline
+			${lib.getExe go} mod tidy
+		'';
 	};
-
-	patchPhase = ''
-		git apply -p1 go-mod.patch
-	'';
 
 	vendorHash = "sha256-mvUnt+CQ02EPzrCTgf6Hcg5L9O2FWasLpFh/okk5tak=";
 

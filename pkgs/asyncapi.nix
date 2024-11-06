6c3819b6815226ac0c2e422b66848f2cd2f7d4d8
William Artero <git@artero.dev>
2024-11-06 13:30:00 +0100

    refactor(asyncapi): use store-imported npm lock deps

--- pkgs/asyncapi.nix
+++ pkgs/asyncapi.nix
@@ -11,11 +11,10 @@ buildNpmPackage rec {
 		hash = "sha256-gbA2/O5Q18uHyCZX99cPfpGMTYqffyR6sisEmeFLy6A=";
 	};
 
-	## TODO disable sandbox in MacOS to use this
-	# npmDeps = importNpmLock {
-	# 	npmRoot = src;
-	# };
-	# npmConfigHook = importNpmLock.npmConfigHook;
+	npmDeps = importNpmLock {
+		npmRoot = src;
+	};
+	npmConfigHook = importNpmLock.npmConfigHook;
 
 	npmDepsHash = "sha256-tZgnKJW49/y1+hSBjwFXCJrplBkkL87Bux7LqOoNSAg=";
 	npmPackFlags = [ "--ignore-scripts" ];

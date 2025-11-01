661309121856681f40368c469647826a8901abb1
William Artero <git@artero.dev>
2025-11-01 11:17:00 +0100

    refactor: use flake
    ps-id: 4f621471-a91c-4476-9d4e-b26a2d6e6cd1
    Signed-off-by: William Artero <git@artero.dev>
    
    # Conflicts:
    #	default.nix

--- pkgs/pkgsite.nix
+++ pkgs/pkgsite.nix
@@ -1,29 +1,33 @@
-{ buildGoModule, fetchFromGitHub, lib }:
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+}:
 
 buildGoModule rec {
-	pname = "pkgsite";
-	version = "0.0.0+${rev}";
-	rev = "557c002897fca2516fc696a6a39fa978ff5a719a";
+  pname = "pkgsite";
+  version = "0.0.0+${rev}";
+  rev = "557c002897fca2516fc696a6a39fa978ff5a719a";
 
-	src = fetchFromGitHub {
-		inherit rev;
-		owner = "golang";
-		repo = "pkgsite";
-		hash = "sha256-RM1I5FeM8ZHn2AmSx5kUD/IqyTi4Up6Ab/j1FJ+ZfYU=";
-	};
+  src = fetchFromGitHub {
+    inherit rev;
+    owner = "golang";
+    repo = "pkgsite";
+    hash = "sha256-RM1I5FeM8ZHn2AmSx5kUD/IqyTi4Up6Ab/j1FJ+ZfYU=";
+  };
 
-	vendorHash = "sha256-/+w3a+xOr4QrqTg7MSJlDh7XTeAPafqHtlAeSdNKqv8=";
+  vendorHash = "sha256-/+w3a+xOr4QrqTg7MSJlDh7XTeAPafqHtlAeSdNKqv8=";
 
-	subPackages = [
-		"cmd/pkgsite"
-	];
+  subPackages = [
+    "cmd/pkgsite"
+  ];
 
-	meta = with lib; {
-		description = "a site for discovering Go packages";
-		homepage = "https://cs.opensource.google/go/x/pkgsite";
-		license = licenses.bsd3;
-		mainProgram = "pkgsite";
-		maintainers = with maintainers; [ wwmoraes ];
-		platforms = platforms.all;
-	};
+  meta = with lib; {
+    description = "a site for discovering Go packages";
+    homepage = "https://cs.opensource.google/go/x/pkgsite";
+    license = licenses.bsd3;
+    mainProgram = "pkgsite";
+    maintainers = with maintainers; [ wwmoraes ];
+    platforms = platforms.all;
+  };
 }
